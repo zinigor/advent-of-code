d2b2dd1b0a94164c19b701a963aa0a8524872b7d
Igor Zinovyev <zinigor@gmail.com>
2023-12-16 22:31:01 +0300

    Day 13 part one.

--- day13.php
+++ day13.php
@@ -4,60 +4,103 @@ require './input13.php';
 
 class MirrorMap {
 
-    public array $rows = [];
+	public array $rows = [];
 
-    public array $columns = [];
+	public array $columns = [];
 
-    public function __construct( $input ) {
-        $rows = explode( "\n", $input );
+	public array $mirror = [];
+
+	public function __construct( $input ) {
+		$rows = explode( "\n", $input );
 
 		foreach ( $rows as $row ) {
 			$this->rows[] = $row;
 
-			foreach( str_split( $row, 1 ) as $index => $char ) {
+			foreach ( str_split( $row, 1 ) as $index => $char ) {
 				$this->columns[ $index ] ??= '';
 				$this->columns[ $index ] .= $char;
 			}
 		}
 
-		print_r( $this->rows );
+		//print_r( $this->rows );
+		//print_r( $this->columns );
 
-		foreach ( $this->rows as $row ) {
-			print_r( $this->find_middle( $row ) );
+		$horizontal_reflections = [];
+		foreach ( $this->rows as $index => $row ) {
+			$horizontal_reflections[ $index ] = $this->find_middle( $row );
 		}
+
+		$vertical_reflections = [];
+		foreach ( $this->columns as $index => $column ) {
+			$vertical_reflections[ $index ] = $this->find_middle( $column );
+		}
+
+		$horizontal = array_reduce( $horizontal_reflections, array( $this, 'reduce_reflections' ), 0 );
+		$vertical = array_reduce( $vertical_reflections, array( $this, 'reduce_reflections' ), 0 );
+
+		$this->mirror = [
+			$horizontal[0] ?? null,
+			$vertical[0] ?? null,
+		];
 	}
 
-	public function find_middle( $row ) {
+	public function reduce_reflections( $carry, $item ) {
+		if ( 0 === $carry ) {
+			return $item;
+		}
+
+		$result = [];
+		foreach ( $carry as $carry_item ) {
+			if ( in_array( $carry_item, $item ) ) {
+				$result[] = $carry_item;
+			}
+		}
+
+		return $result;
+	}
+
+	public function find_middle( $row ): array {
+		$possibilities = [];
 		$length = strlen( $row );
-		for ( $i = 0; $i < $length; $i += 2 ) {
+		//printf( "--------------\n%s\n", $row );
+		for ( $i = $length % 2; $i < $length; $i += 2 ) {
+			for ( $end = 0; $end < 2; $end++ ) {
+				//print( "Start: $end; Index: $i\n" );
+				if ( $end ) {
+					$start_chunk = substr( $row, $i );
+				} else {
+					$start_chunk = substr( $row, 0, $length - $i );
+				}
+				$start_chunk_backwards = strrev( $start_chunk );
+				//printf( "%s\n", $start_chunk );
 
-			printf( "Checking: %s\n", substr( $row, $i ) );
-			// Checking from the left.
-			printf(
-				"Checking: \n%s vs \n%s\n",
-				substr( $row, 0, $length - $i ),
-				substr( strrev( $row ), $i )
-			);
-			if (
-				substr( $row, 0, $length - $i ) === substr( strrev( $row ), $i )
-			) {
-				print "MATCH\n";
-				return ( strlen( $row ) - $i ) / 2;
-			}
-
-			printf(
-				"Checking: \n%s vs \n%s\n",
-				substr( $row, $i ),
-				substr( strrev( $row ), 0 )
-			);
-			if ( substr( $row, $i ) === substr( strrev( $row ), 0, $length - $i ) ) {
-				return ( strlen( $row) + $i ) / 2;
+				if ( $start_chunk === $start_chunk_backwards ) {
+					//printf( "MATCH %f\n", $i + ( strlen( $start_chunk ) / 2 ) );
+					$key = $end ?
+							 $i + ( strlen( $start_chunk ) / 2 ) :
+							 ( strlen( $start_chunk ) / 2 );
+					// printf( "Match: \t%s\nIteration: %d, Key: %d\n", $start_chunk, $i, $key);
+					$possibilities[] = $key;
+				}
 			}
 		}
+		return $possibilities;
+	}
+
+	public function get_mirror() {
+		return $this->mirror;
 	}
 }
 
+$sum = 0;
 foreach ( explode( "\n\n", $input ) as $block ) {
-	new MirrorMap( $block );
-    break;
+	$mirrormap = new MirrorMap( $block );
+	$map = $mirrormap->get_mirror();
+	if ( ! is_null( $map[0] ) ) {
+		$sum += $map[0];
+	} else {
+		$sum += $map[1] * 100;
+	}
 }
+
+printf("%d\n", $sum);

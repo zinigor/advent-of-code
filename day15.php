f4523aa2ea1262d10feb8fded5870058d0da2617
Igor Zinovyev <zinigor@gmail.com>
2023-12-20 17:42:24 +0300

    Added a box class.

--- day15.php
+++ day15.php
@@ -33,6 +33,90 @@ class Hasher {
     }
 }
 
+class Box {
+
+    protected array $lenses = [];
+
+    protected array $ids = [];
+
+    public function add_lens( $id, $focal ) {
+        if ( $this->has_lens( $id ) ) {
+            $this->replace_lens( $id, $focal );
+        } else {
+            $this->push_lens( $id, $focal );
+        }
+    }
+
+    public function has_lens( $id ): bool {
+        return isset( $this->ids[ $id ] );
+    }
+
+    public function remove_lens( $id ): void {
+
+        if ( ! $this->has_lens( $id ) ) {
+            throw new Exception( "No such lens with id: " . $id );
+        }
+
+        unset( $this->lenses[ $this->ids[ $id ] ] );
+        unset( $this->ids[ $id ] );
+
+        // Compacting ids.
+        $this->lenses = array_values( $this->lenses );
+    }
+
+    public function replace_lens( $id, $focal ): void {
+        if ( ! $this->has_lens( $id ) ) {
+            throw new Exception( "No such lens with id: " . $id );
+        }
+
+        $this->lenses[ $this->ids[ $id ] ] = [
+            'id' => $id,
+            'focal' => $focal
+        ];
+    }
+
+    public function push_lens( $id, $focal ): void {
+        if ( $this->has_lens( $id ) ) {
+            throw new Exception( "Lens with such id already added: " . $id );
+        }
+
+        array_push(
+            $this->lenses,
+            [
+                'id' => $id,
+                'focal' => $focal
+            ]
+        );
+
+        $this->generate_ids();
+    }
+
+    public function generate_ids(): void {
+        $this->ids = [];
+        foreach ( $this->lenses as $index => $lens ) {
+            $this->ids[ $lens['id'] ] = $index;
+        }
+    }
+
+    /**
+     * Adds up the focusing power of all of the lenses.
+     * The focusing power of a single lens is the result of multiplying together:
+     *
+     *      The slot number of the lens within the box: 1 for the first lens, 2 for the second lens, and so on.
+     *      The focal length of the lens.
+     *
+     * @param int $box the number of the box.
+     */
+    public function get_focus_power( $box ): int {
+        $sum = 0;
+        foreach ( $this->lenses as $index => $lens ) {
+            $sum += $box * ( $index + 1 ) * $lens['focal'];
+        }
+
+        return $sum;
+    }
+}
+
 $parts = explode( ',', $input );
 //print_r( $parts );
 // print_r( Hasher::hash( $parts[0] ) );
@@ -41,30 +125,35 @@ $sum = 0;
 $boxes = [];
 $lenses = [];
 foreach ( $parts as $part ) {
-    if ( false !== strpos( $part, '=' ) ) {
+    if ( '=' === $part[-2] ) {
         $focal = $part[-1];
         $label = substr( $part, 0, strlen( $part ) - 2 );
-        printf( "Part: %s\nMarking lens %s as focal length %d\nBox: %d\n", $part, $label, $focal, $hash );
         $box = Hasher::hash( $label );
+        printf( "Part: %s\tMarking %s length %d\tBox: %d\n", $part, $label, $focal, $box );
 
-        $lenses[ $label ] = $focal;
-        $boxes[ $box ] ??= [];
-        $lenses[ $box ] ??= [];
+        $boxes[ $box ] ??= new Box();
+        $boxes[ $box ]->add_lens( $label, $focal );
+    } elseif ( '-' === $part[-1] ) {
+        $label = substr( $part, 0, strlen( $part ) - 1 );
+        $box = Hasher::hash( $label );
+        printf( "Part: %s\tRemoving lens %s\tBox: %d\n", $part, $label, $box );
 
-        if ( false !== ( $index = in_array( $label, $boxes[ $box ] ) ) ) {
-            $new_box = [];
-            $new_focals = [];
-            foreach ( $boxes[ $box ] as $index => $item ) {
+        $boxes[ $box ] ??= new Box();
 
-            } 
-        } else {
-            array_unshift( $boxes[ $box ], $label );
-            $lenses[ $box ][ 0 ] = $focal;
+        try {
+            $boxes[ $box ]->remove_lens( $label, $focal );
+        } catch ( Exception $e ) {
+            //printf( "No such lens, carrying on.\n" );
         }
     } else {
-
+        throw new Exception ( 'WHF' );
     }
 }
 
+$sum = 0;
+foreach( $boxes as $index => $box ) {
+    $sum += $box->get_focus_power( $index + 1 );
+}
+
 printf( "Sum: %d\n", $sum );
-print_r( $boxes );
+//print_r( $boxes );

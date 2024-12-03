75c1e252f437d1c0145ab68d34b7fc2e49695061
Igor Zinovyev <zinigor@gmail.com>
2024-12-03 15:56:55 +0300

    Day 2 fixed.

--- 2024/02/task.php
+++ 2024/02/task.php
@@ -36,58 +36,14 @@ class Report {
 				// echo "Going down" . PHP_EOL;
 				$new_direction = -1;
 			} else {
-				if ( $this->dampener ) {
-					echo "Exiting with dampener on at $level because zero movement" . PHP_EOL;
-					return true;
-				} elseif ( isset ( $this->dampened_object ) ) {
-					return $this->dampened_object->has_breaks();
-				} else {
-
-					$dampener_data = array_diff_key( $this->levels, array( $index => $level ) );
-					$this->dampened_object = new Report( join( ' ', $dampener_data ), true );
-					echo "Activating zero dampener for: " . join( '-', $this->levels ) . " at $level ". PHP_EOL;
-					echo "Dampener data: " . join( ' ', $this->dampened_object->levels ) . PHP_EOL;
-
-					return $this->dampened_object->has_breaks();
-				}
-
+				return true;
 			}
 
 			if ( ! isset( $direction ) ) {
 				// echo "No previous direction, current " . $new_direction . PHP_EOL;
  				$direction = $new_direction;
 			} elseif ( $new_direction !== $direction ) {
-				if ( $this->dampener ) {
-					echo "Exiting with dampener on at $level because diffent direction" . PHP_EOL;
-					return true;
-				} elseif ( isset ( $this->dampened_object ) ) {
-					return $this->dampened_object->has_breaks();
-				} else {
-					$dampener_data = array_diff_key( $this->levels, array( ( $index - 1 ) => $level ) );
-					$left_dampener = new Report( join( ' ', $dampener_data ), true );
-					$dampener_data = array_diff_key( $this->levels, array( ( $index ) => $level ) );
-					$right_dampener = new Report( join( ' ', $dampener_data ), true );
-
-					$left_turn = $left_dampener->has_breaks();
-					$right_turn = $right_dampener->has_breaks();
-
-					if ( ! $left_turn ) {
-						$this->dampened_object = $left_dampener;
-						echo "Activating direction dampener for: " . join( '-', $this->levels ) . " at $level - 1 ". PHP_EOL;
-						echo "Dampener data: " . join( ' ', $left_dampener->levels ) . PHP_EOL;
-
-						return false;
-					} elseif ( ! $right_turn ) {
-						$this->dampened_object = $right_dampener;
-						echo "Activating direction dampener for: " . join( '-', $this->levels ) . " at $level ". PHP_EOL;
-						echo "Dampener data: " . join( ' ', $right_dampener->levels ) . PHP_EOL;
-
-						return false;
-					}
-					echo "Direction dampener didn't help for: " . join( '-', $this->levels ) . " at $level ". PHP_EOL;
-
-					return true;
-				}
+				return true;
 			}
 
 			$previous = $level;
@@ -109,37 +65,7 @@ class Report {
 			// echo "Current level $level ";
 			// echo "Previous level $previous " . PHP_EOL;
 			if ( max( $level, $previous) - min( $level, $previous ) > 3 ) {
-				if ( $this->dampener ) {
-					echo "Exiting with dampener on at $level because spike" . PHP_EOL;
-					return true;
-				} elseif ( isset ( $this->dampened_object ) ) {
-					return $this->dampened_object->has_spikes();
-				} else {
-					$dampener_data = array_diff_key( $this->levels, array( ( $index - 1 ) => $level ) );
-					$left_dampener = new Report( join( ' ', $dampener_data ), true );
-					$dampener_data = array_diff_key( $this->levels, array( ( $index ) => $level ) );
-					$right_dampener = new Report( join( ' ', $dampener_data ), true );
-
-					$left_spike = $left_dampener->has_spikes();
-					$right_spike = $right_dampener->has_spikes();
-
-					if ( ! $left_spike ) {
-						$this->dampened_object = $left_dampener;
-						echo "Activating spike dampener for: " . join( '-', $this->levels ) . " at $level ". PHP_EOL;
-						echo "Dampener data: " . join( ' ', $left_dampener->levels ) . PHP_EOL;
-
-						return false;
-					} elseif ( ! $right_spike ) {
-						$this->dampened_object = $right_dampener;
-						echo "Activating spike dampener for: " . join( '-', $this->levels ) . " at $level ". PHP_EOL;
-						echo "Dampener data: " . join( ' ', $right_dampener->levels ) . PHP_EOL;
-
-						return false;
-					}
-					echo "Spike dampener didn't help: " . join( '-', $this->levels ) . " at $level ". PHP_EOL;
-
-					return true;
-				}
+				return true;
 			}
 
 			$previous = $level;
@@ -149,7 +75,24 @@ class Report {
 	}
 
 	public function is_safe() {
-		return ! $this->has_breaks() && ! $this->has_spikes();
+		$is_safe = ! $this->has_breaks() && ! $this->has_spikes();
+
+		if ( ! $is_safe ) {
+			for ( $i = 0; $i < sizeof( $this->levels ); $i++ ) {
+				$new_levels = array_diff_key(
+					$this->levels,
+					array( $i => 1 )
+				);
+
+				$report = new Report( join( ' ', $new_levels ) );
+
+				if ( ! $report->has_breaks() && ! $report->has_spikes() ) {
+					return true;
+				}
+			}
+		}
+
+		return $is_safe;
 	}
 }
 
@@ -160,14 +103,14 @@ foreach ( $input as $data ) {
 	//$breaks = $report->has_breaks();
 	//$spikes = $report->has_spikes();
 	//echo "Report $data: " . PHP_EOL;
-	echo "----------------" . PHP_EOL;
+	// echo "----------------" . PHP_EOL;
 	$is_safe = $report->is_safe();
 	//echo "Has breaks: " . ( $breaks ? 'yes' : 'no' )  . PHP_EOL;
 	//echo "Has spikes: " . ( $spikes ? 'yes' : 'no' )  . PHP_EOL;
 	// echo "Is safe: " . ( $is_safe ? 'yes' : 'no' ) . PHP_EOL;
 
 	if ( $is_safe ) {
-		echo "Is safe!" . PHP_EOL;
+		// echo "Is safe!" . PHP_EOL;
 		$safe_count++;
 	}
 }
